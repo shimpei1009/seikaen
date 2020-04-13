26c834efc2aa52e0dcb0642e3f088f4423ab9618
shimpei1009 <gogo.ikeike.1009@icloud.com>
2020-04-13 23:27:13 +0900

    edit js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,6 +2,7 @@ var elInner, duration, defaultIndex, switchImage;
 elInner = document.getElementsByClassName('wrapper__photo__image');
 duration = 5000;
 defaultIndex = 0;
+  if(elInner.length) {
 switchImage = function(next) { 
   var current = next ? (next - 1) : elInner.length - 1;
   elInner[current].classList.remove('is-visible');
@@ -10,3 +11,4 @@ switchImage = function(next) {
   setTimeout(switchImage.bind(this, next), duration);
 };
 window.onload = switchImage.bind(this, defaultIndex);
+};
