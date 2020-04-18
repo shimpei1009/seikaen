fe87dac497fe7b76edfdbc709e7567e6588cbb30
shimpei1009 <gogo.ikeike.1009@icloud.com>
2020-04-19 02:02:09 +0900

    made map

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,22 @@
+var myMap = new google.maps.Map(document.getElementById("map"), {
+  // ズームレベル
+  zoom: 17,
+  // 中心点緯度経度
+  center: new google.maps.LatLng(35.6853812, 139.8004000),
+  // 距離目盛りの表示
+  scaleControl: true,
+  // 地図の種類
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+var myMarker = new google.maps.Marker({
+  // マーカーを置く緯度経度
+  position: new google.maps.LatLng(35.6853812, 139.8004000),
+  map: myMap
+});
+
+
+google.maps.event.addListener(myInfoWindow, "closeclick", function() {
+  google.maps.event.addListenerOnce(myMarker, "click", function(event) {
+    myInfoWindow.open(myMap, myMarker);
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
   get 'info', to: 'info#index'
   resources :info, only: [:index, :edit, :update, :destroy]
+  get 'maps', to: 'maps#index'
   get  'form' =>'messages#form'
   post 'confirm' => 'messages#confirm'
   post 'done' => 'messages#done'
